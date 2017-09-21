b1b5a09a7401c4fa24720747dc75f81732e7babf
Gengjie <chengengjie93@gmail.com>
2017-09-21 20:02:02 +0800

    add base_operator

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# small-cpp-exp
-Small C++ Experiments
+# Small C++ Experiments
+
+Notes:
+* Each cpp file corresponds to a single experiment and contains its own main function.
+* Compiler: gcc-6.3.0
---
+++ base_operator.cpp
@@ -0,0 +1,50 @@
+// Check how to call the operator of base class from derived class
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Base
+{
+public:
+    Base(int x) : _x(x) {}
+    
+    bool operator ==(const Base& rhs)
+    {
+        return _x == rhs._x;
+    }
+
+private:
+    int _x;
+};
+
+class Derived : public Base
+{
+public:
+    Derived(int x, int y) : Base(x), _y(y) {}
+
+    bool operator ==(const Derived& rhs)
+    {
+        return Base::operator==(rhs) && _y == rhs._y;
+    }
+
+private:
+    int _y;
+};
+
+int main()
+{
+    Derived a(1, 1), b(1, 1), c(1, 2), d(2, 2);
+    cout << (a==b) << endl; // true
+    cout << (a==c) << endl; // false
+    cout << (a==d) << endl; // false
+
+    vector<int> vecInts = {1, 2, 3};
+    for (auto val : vecInts)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
