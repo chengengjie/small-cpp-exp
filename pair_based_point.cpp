cae8ebd95b2357d85eab1390cf28f9e441642f0b
Gengjie <chengengjie93@gmail.com>
2017-09-27 17:32:02 +0800

    add pair_based_point

---
+++ pair_based_point.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <utility>
+#include <chrono>
+
+using namespace std;
+
+class Point : public pair<int, int>
+{
+public:
+    using pair<int, int>::pair;
+
+    int x() { return first; }
+    int y() { return second; }
+    inline int x_inline() { return first; }
+};
+
+int main()
+{
+    Point ptA(10, 8), ptB(10, 8), ptC(10, 10);
+
+    // use get<index>(pair)
+    cout << ptA.x() << endl;
+    cout << get<0>(ptA) << endl;
+
+    // use pair::operator==
+    cout << boolalpha;
+    cout << (ptA == ptB) << endl;
+    cout << (ptB == ptC) << endl;
+
+    // auto ptD = ptA + ptB;    // invalid
+
+    // no runtime diff between x() and x_inline()
+    using namespace chrono;
+    const long long times = 1e9;
+    using clock = high_resolution_clock ;
+    Point pt(1, 1);
+    {
+        auto start = clock::now();
+        int totX = 0;
+        for (long long i = 0; i < times; ++i)
+        {
+            totX += pt.x();
+            totX -= pt.x();
+        }
+        duration<double> dur = clock::now() - start;
+        cout << dur.count() << endl;
+    }
+    {
+        auto start = clock::now();
+        int totX = 0;
+        for (long long i = 0; i < times; ++i)
+        {
+            totX += pt.x_inline();
+            totX -= pt.x_inline();
+        }
+        duration<double> dur = clock::now() - start;
+        cout << dur.count() << endl;
+    }
+}
