5c7c2e44cfb50e47a89c0b798d43860ba16dfca7
Gengjie <chengengjie93@gmail.com>
2017-12-14 20:20:21 +0800

    add std_exception_handling

--- pair_based_point.cpp
+++ pair_based_point.cpp
@@ -14,46 +14,68 @@ public:
     inline int x_inline() { return first; }
 };
 
+class PointRaw
+{
+public:
+    int x, y;
+
+    PointRaw(int xx, int yy) : x(xx), y(yy) {}
+};
+
 int main()
 {
-    Point ptA(10, 8), ptB(10, 8), ptC(10, 10);
-
-    // use get<index>(pair)
-    cout << ptA.x() << endl;
-    cout << get<0>(ptA) << endl;
-
-    // use pair::operator==
-    cout << boolalpha;
-    cout << (ptA == ptB) << endl;
-    cout << (ptB == ptC) << endl;
-
-    // auto ptD = ptA + ptB;    // invalid
-
-    // no runtime diff between x() and x_inline()
-    using namespace chrono;
-    const long long times = 1e9;
-    using clock = high_resolution_clock ;
-    Point pt(1, 1);
     {
-        auto start = clock::now();
-        int totX = 0;
-        for (long long i = 0; i < times; ++i)
+        Point ptA(10, 8), ptB(10, 8), ptC(10, 10);
+
+        // use get<index>(pair)
+        cout << ptA.x() << endl;
+        cout << get<0>(ptA) << endl;
+
+        // use pair::operator==
+        cout << boolalpha;
+        cout << (ptA == ptB) << endl;
+        cout << (ptB == ptC) << endl;
+
+        // auto ptD = ptA + ptB;    // invalid
+
+        // no runtime diff between x() and x_inline()
+        using namespace chrono;
+        const long long times = 1e9;
+        using clock = high_resolution_clock ;
+        Point pt(1, 1);
         {
-            totX += pt.x();
-            totX -= pt.x();
+            auto start = clock::now();
+            int totX = 0;
+            for (long long i = 0; i < times; ++i)
+            {
+                totX += pt.x();
+                totX -= pt.x();
+            }
+            duration<double> dur = clock::now() - start;
+            cout << dur.count() << endl;
         }
-        duration<double> dur = clock::now() - start;
-        cout << dur.count() << endl;
-    }
-    {
-        auto start = clock::now();
-        int totX = 0;
-        for (long long i = 0; i < times; ++i)
         {
-            totX += pt.x_inline();
-            totX -= pt.x_inline();
+            auto start = clock::now();
+            int totX = 0;
+            for (long long i = 0; i < times; ++i)
+            {
+                totX += pt.x_inline();
+                totX -= pt.x_inline();
+            }
+            duration<double> dur = clock::now() - start;
+            cout << dur.count() << endl;
         }
-        duration<double> dur = clock::now() - start;
-        cout << dur.count() << endl;
     }
+
+    // No default operator== and operator!= ...
+    // {
+    //     PointRaw ptA(10, 8), ptB(10, 8), ptC(10, 10);
+
+    //     // use operator==
+    //     cout << boolalpha;
+    //     cout << (ptA == ptB) << endl;
+    //     cout << (ptA != ptB) << endl;
+    //     cout << (ptB == ptC) << endl;
+    //     cout << (ptB != ptC) << endl;
+    // }
 }
---
+++ std_exception_handling.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+    std::ifstream f("doesn't exist");
+    try {
+        f.exceptions(f.failbit);
+    }
+    catch (const std::ios_base::failure& e) {
+        std::cout << "Caught an ios_base::failure.\n"
+                  << "Explanatory string: " << e.what() << '\n'
+                  << "Error code: " << e.code() << '\n';
+    }
+}
